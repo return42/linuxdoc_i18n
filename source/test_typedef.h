354f8b83d7a4f60501cc2ebbf74c55cd3fb02f81
Markus Heiser <markus.heiser@darmarit.de>
2021-11-13 16:31:35 +0100

    [enh] add test_typedef.h - realted to linuxdoc PR-13
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- source/test_typedef.h
+++ source/test_typedef.h
@@ -4,3 +4,21 @@
  * @b: The number B.
  */
 typedef void (*sum)(int a, int b);
+
+
+/**
+ * typedef genpool_algo_t: Allocation callback function type definition
+ * @map: Pointer to bitmap
+ * @size: The bitmap size in bits
+ * @start: The bitnumber to start searching at
+ * @nr: The number of zeroed bits we're looking for
+ * @data: optional additional data used by the callback
+ * @pool: the pool being allocated from
+ */
+typedef unsigned long (*genpool_algo_t)(
+	unsigned long *map,
+	unsigned long size,
+	unsigned long start,
+	unsigned int nr,
+	void *data, struct gen_pool *pool,
+	unsigned long start_addr);
